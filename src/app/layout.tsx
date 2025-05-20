49a6ef8013e2da0c37f13f4a2a298386badcc57c
Adão com H <adaocomh@gmail.com>
2025-05-20 09:16:06 -0300

    Remove fontes não utilizadas do layout e simplifica a classe do corpo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "É. Adão Dev.",
@@ -29,7 +18,7 @@ export default function RootLayout({
     <html lang="pt" className="translated-ltr">
       <body
         cz-shortcut-listen="true"
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={'antialiased'}
       >
         {children}
       </body>
