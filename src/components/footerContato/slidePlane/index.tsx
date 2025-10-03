71ff1aacc1dfb739685de842cfa76824926f60aa
Adão com H <adaocomh@gmail.com>
2025-10-03 07:50:47 -0300

    Refatora o componente SlidePlane para melhorar a transição de visibilidade, ajustando a posição e a opacidade com base no estado de visibilidade.

--- src/components/footerContato/slidePlane/index.tsx
+++ src/components/footerContato/slidePlane/index.tsx
@@ -6,7 +6,7 @@ export function SlidePlane(){
     return(
         <SlideEffect<HTMLImageElement>>
         {(ref, visivel) => (
-        <div ref={ref} className={`${visivel ?'bg-[#F5ECDB] w-[200px] h-[200px] rounded-[50%] translate-x-[0] transition-all duration-1800 opacity-100' : ' transition-all duration-1800 opacity-0'}`}>
+        <div ref={ref} className={`bg-[#F5ECDB] w-[200px] h-[200px] rounded-[50%] transition-all duration-1800 ${visivel ?'translate-x-[0] opacity-100' : 'translate-x-[-200px] opacity-0'}`}>
             <LottiePlane/>
         </div>
         )}
