e83b3fada630b3e6e5ace40eee69b5a035d3829c
Adão com H <adaocomh@gmail.com>
2025-10-05 14:06:59 -0300

    Efeito de "vidro" no menu suspenso.

--- src/components/mnSuspenso/index.tsx
+++ src/components/mnSuspenso/index.tsx
@@ -51,16 +51,16 @@ export default function MenuSuspenso() {
 
   return (
     <div className="flex justify-between items-center w-[100vw] p-[0px_80px] transition-all duration-500">
-      <div className={`hover:translate-y-[-4px] hover:transition-all hover:shadow-[0px_3px_8px_rgba(0,0,0,0.3)] p-[20px] rounded-[50px] transition-all duration-600 ${overFooter ? "bg-transparent backdrop-blur-md text-(--cor-primaria) border-[0.1px] borde-(--cor-primaria) shadow-[0px_8px_10px_rgba(0,0,0,0.10)]" : scrolled ? "bg-transparent backdrop-blur-md shadow-[0px_8px_10px_rgba(0,0,0,0.05)]" : ""}`}>
+      <div className={`hover:translate-y-[-4px] hover:transition-all hover:shadow-[inset_2px_2px_8px_rgba(255,255,255,0.08),00px_3px_8px_rgba(0,0,0,0.3)] p-[20px] rounded-[50px] transition-all duration-600 ${overFooter ? "bg-transparent backdrop-blur-md text-(--cor-primaria) shadow-[inset_2px_2px_8px_rgba(255,255,255,0.08),0px_8px_10px_rgba(0,0,0,0.08)]" : scrolled ? "bg-transparent backdrop-blur-md  shadow-[inset_2px_2px_8px_rgba(255,255,255,0.1),0px_8px_10px_rgba(0,0,0,0.08)]" : ""}`}>
         <p className="cursor-pointer font-light">
           ⓒ Code by É. Adão
         </p>
       </div>
       <nav>
-        <ul className={`hidden rounded-[50px] md:flex md:flex-row md:gap-[20px] md:p-[20px] transition-all duration-600 ${overFooter ? "bg-transparent backdrop-blur-md text-(--cor-primaria) border-[0.1px] borde-(--cor-primaria) shadow-[0px_8px_10px_rgba(0,0,0,0.10)]" : scrolled ? "bg-transparent backdrop-blur-md shadow-[0px_8px_10px_rgba(0,0,0,0.05)]" : ""}`}>
-          <li className="cursor-pointer hover:translate-y-[-1px] hover:transition-all hover:duration-300 hover:text-shadow-[0px_5px_15px_rgba(0,0,0,0.3)]" onClick={() => scrollSection("sS2")}>Meu objetivo</li>
-          <li className="cursor-pointer hover:translate-y-[-1px] hover:transition-all hover:duration-300 hover:text-shadow-[0px_5px_15px_rgba(0,0,0,0.3)]" onClick={() => scrollSection("aPS2")}>Demo.</li>
-          <li className="cursor-pointer hover:translate-y-[-1px] hover:transition-all hover:duration-300 hover:text-shadow-[0px_5px_15px_rgba(0,0,0,0.3)]" onClick={() => scrollSection("footerS4")}>Contato</li>
+        <ul className={`hidden rounded-[50px] md:flex md:flex-row md:gap-[20px] md:p-[20px] transition-all duration-600 ${overFooter ? "bg-transparent backdrop-blur-md text-(--cor-primaria) shadow-[inset_2px_2px_8px_rgba(255,255,255,0.08),0px_8px_10px_rgba(0,0,0,0.08)]" : scrolled ? "bg-transparent backdrop-blur-md shadow-[inset_2px_2px_8px_rgba(255,255,255,0.1),0px_8px_10px_rgba(0,0,0,0.08)]" : ""}`}>
+          <li className="cursor-pointer hover:translate-y-[-1px] hover:transition-all hover:duration-500 hover:text-shadow-[0px_5px_15px_rgba(0,0,0,0.3)]" onClick={() => scrollSection("sS2")}>Meu objetivo</li>
+          <li className="cursor-pointer hover:translate-y-[-1px] hover:transition-all hover:duration-500 hover:text-shadow-[0px_5px_15px_rgba(0,0,0,0.3)]" onClick={() => scrollSection("aPS2")}>Demo.</li>
+          <li className="cursor-pointer hover:translate-y-[-1px] hover:transition-all hover:duration-500 hover:text-shadow-[0px_5px_15px_rgba(0,0,0,0.3)]" onClick={() => scrollSection("footerS4")}>Contato</li>
         </ul>
         <MnSmobile />
       </nav>
