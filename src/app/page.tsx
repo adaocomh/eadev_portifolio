3f25961c977758a2228615d2a35b6683bc410fde
Adão com H <adaocomh@gmail.com>
2025-10-05 09:58:20 -0300

    Ajuste no padding...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import MenuSuspenso from '../components/mnSuspenso'
 export default function Home() {
   return (
     <>
-        <div className='fixed top-[40px] h-[100px] z-50'><MenuSuspenso/></div>
+        <div className='fixed top-[20px] h-[100px] z-50'><MenuSuspenso/></div>
         <Header/>
         <MeuObjetivo/>
         <Demo/>
