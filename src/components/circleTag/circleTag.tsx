60d11cf0913b52f03b3bb295a79ee3553b559f86
Adão com H <adaocomh@gmail.com>
2025-10-25 12:10:49 -0300

    Componente "circleTag" refatorado.

--- src/components/circleTag/circleTag.tsx
+++ src/components/circleTag/circleTag.tsx
@@ -3,40 +3,31 @@ import { useEffect, useState } from "react";
 
 export default function TextCircle() {
   const [letters, setLetters] = useState<string[]>([]);
-  const [translateZ, setTranslateZ] = useState(80); // valor padrão (mobile)
+  const [translateZ, setTranslateZ] = useState(80);
   const content = "© code by É. Adão. ";
 
   useEffect(() => {
     setLetters(content.split(""));
 
-    // Atualiza automaticamente quando muda o tamanho da tela
     const handleResize = () => {
-      setTranslateZ(window.innerWidth >= 768 ? 120 : 80); // md: equivale a 768px
+      setTranslateZ(window.innerWidth >= 768 ? 120 : 80);
     };
 
-    handleResize(); // executa na montagem
+    handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  // Função que aplica o transform baseado na tela
   const getTransform = (deg: number) => {
     return `rotateY(${deg}deg) translateZ(${translateZ}px)`;
   };
 
   return (
     <div className="relative w-[300px] h-[300px] flex items-center justify-center">
-      <div
-        className="m-auto origin-center"
-        style={{
+      <div className="m-auto origin-center" style={{
           transform: "rotateX(20deg) rotate(-20deg)",
-          transformStyle: "preserve-3d",
-        }}
-      >
-        <div
-          className="uppercase font-medium text-[30px] md:text-[50px] text-[#555936D8] animate-[spinY_10s_linear_infinite]"
-          style={{ transformStyle: "preserve-3d" }}
-        >
+          transformStyle: "preserve-3d"}}>
+        <div className="uppercase font-medium text-[30px] text-[#555936D8] animate-[spinY_10s_linear_infinite] md:text-[50px]" style={{ transformStyle: "preserve-3d" }}>
           {letters.map((l, i) => {
             const step = 360 / letters.length;
             const deg = step * i;
@@ -48,8 +39,6 @@ export default function TextCircle() {
           })}
         </div>
       </div>
-
-      {/* Keyframes custom */}
       <style jsx>{`
         @keyframes spinY {
           0% {
